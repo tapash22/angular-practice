2d393faec2e11bd64f2e39950485e07a991a79db
tapash22 <tapasp263@gmail.com>
2025-03-15 17:44:42 +0600

    update with adding portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+image1 = './images/p.jpg'
+image2 = './images/p1.jpg'
+image3 = './images/p2.jpg'
+
+
+demoList:any[]=[
+  {
+    id:1,
+    name:'development',
+    description:'this my website into this i added vue.js tailwind and animation also implement 3d',
+    image:this.image1,
+    count:65
+  },
+  {
+    id:2,
+    name:'development',
+    description:'this my website into this i added vue.js tailwind and animation also implement 3d',
+    image:this.image2,
+    count:35
+  },
+  {
+    id:3,
+    name:'development',
+    description:'this my website into this i added vue.js tailwind and animation also implement 3d',
+    image:this.image3,
+    count:55
+  }
+]
 
 }
