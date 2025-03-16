ad7a98a9b4928e80bf2cbee861bd2656654e824c
tapash22 <tapasp263@gmail.com>
2025-03-16 23:36:39 +0600

    added contact page with card and form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contact_image = './images/contact.png';
+  email='tapasp263@gmail.com'
+  socialIcons: string[] = ['fa-facebook', 'fa-linkedin', 'fa-square-instagram'];
+
+  formData:any={
+    name:'',
+    email:'',
+    subject:'',
+    message:'',
+    phone:''
+  }
 
 }
