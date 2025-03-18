423c4cad8ba3802f4bd6ef7a63843b6d18979b8e
tapash22 <tapasp263@gmail.com>
2025-03-18 20:38:59 +0600

    update with resume page update

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ResumeComponent {
 
+  downloadFile() {
+    const fileUrl = './cv/cv.pdf'; 
+    const fileName = 'cv.pdf';
+
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
